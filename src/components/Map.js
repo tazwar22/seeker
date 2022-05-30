314e02096ef2c85173fc3edc20008b606a79472d
Ahnaf Tazwar Ayub <ahnafayub@Tazs-MacBook-Air-M1-2.local>
2022-05-30 11:27:34 -0700

    Refactored all functionality in useEffect into functions

--- src/components/Map.js
+++ src/components/Map.js
@@ -14,11 +14,14 @@ library.add(faLocationDot);
 
 
 const Map = () => {
+
+    //Grab the API key form the .env file
+    const KEY = process.env.REACT_APP_TT_API_KEY;
     const mapElement = useRef();
     const [mapOb, setMap] = useState({});
-    const [zoomLevel, setZoomLevel] = useState(18)
+    const [zoomLevel, setZoomLevel] = useState(14)
     const [currpos, setCurrpos] = useState({latitude:49.26609680307735, longitude:-123.24254063087011})
-    const [category, setCategory] = useState('library');
+    const [category, setCategory] = useState('food');
   
     // Function to parse given location
     const parseLatLong = (latLongStr)=>{
@@ -46,13 +49,11 @@ const Map = () => {
           }
         })
       }
-  
-    useEffect(()=>{
-      //Grab the API key form the .env file
-      const KEY = process.env.REACT_APP_TT_API_KEY;
-      // console.log(KEY);
-      // console.log(currpos)
-      //Construct a map
+
+    /*
+    Creates a TomTom Map object
+    */
+    const createMap = () => {
       let tomMap = tt.map({
         key: KEY,
         container: mapElement.current,
@@ -60,132 +61,125 @@ const Map = () => {
         center: [currpos.longitude,  currpos.latitude],
         stylesVisibility: {poi:false} //Remove POI markers
       });
+      return tomMap;
+    };
 
-    //   tomMap.on('click', (ev)=>console.log(ev));
-  
-      setMap(tomMap);
-      // console.log(mapOb);
-  
-      //Add a marker
-      const addMarker = ()=>{
-        const popupOffset = {bottom : [0, -30]};
-        const popup = new tt.Popup({offset : popupOffset}).setHTML("You!");
-  
-        //Make an HTML element
-        const markerElement = document.createElement('div');
-        markerElement.className = 'marker';
-  
-        const marker = new tt.Marker({
-          element:markerElement,
-          draggable:true
-        });
-  
-        //Specify initial position
-        marker.setLngLat([currpos.longitude, currpos.latitude]);
-        // console.log(tomMap);
-        marker.addTo(tomMap);
-  
-         //Add dragging interaction
-        marker.on('dragend', ()=>{
-          // console.log("Finished moving");
-          // console.log(marker.getLngLat());
-          const newPos = marker.getLngLat();
-          setZoomLevel(tomMap.getZoom())
-          console.log("Current zoom level" + zoomLevel)
-          setCurrpos({longitude:newPos.lng, latitude:newPos.lat});
-        })
-  
-        marker.setPopup(popup).togglePopup();
-      }
-  
-      //CALL the function
-      addMarker();
+    /*
+    Add a marker for the user (i.e. origin)
+    */
+    const addMarker = (tomMap) => {
+      const popupOffset = {bottom : [0, -30]};
+      const popup = new tt.Popup({offset : popupOffset}).setHTML("YOU");
 
-        //Adds markers for a given set of points
-        const addMulMarkers = (nearbyPois) => {
-            for (let pid in nearbyPois.results){
-                // console.log(nearbyPois.results[pid]);
-                const point = nearbyPois.results[pid];
-                //Unpack the point object to access the location
-                let poiLoc, poiName;
-                poiLoc = point.position
-                poiName = point.poi.name
-                // console.log(`Name: ${poiName}`);
+      //Make an HTML element
+      const markerElement = document.createElement('div');
+      markerElement.className = 'marker';
+      const marker = new tt.Marker({
+        element:markerElement,
+        draggable:true
+      });
 
-                // Popup
-                const popupOffset = {bottom : [0, -30]};
-                const popup = new tt.Popup({offset : popupOffset}).setHTML(poiName);
-                //Make an HTML element <FontAwesomeIcon icon={FaAmazon} />
-                const markerElement = document.createElement('span', {className:''});
-                markerElement.innerHTML = icon({ prefix: 'fa', iconName: 'location-dot'}).html;
-                markerElement.className = 'marker-poi';
-                // markerElement.addEventListener('click', ()=>{
-                //     console.log("Position...")
-                //     console.log([poiLoc.lng, poiLoc.lat]);
-                // })
-                
-                const marker = new tt.Marker({
-                    element:markerElement,
-                    draggable:false
-                });
-        
-                //Specify initial position
-                marker.setLngLat([poiLoc.lng, poiLoc.lat]);
-                // console.log(marker)
-                marker.setPopup(popup);
-                marker.addTo(tomMap);
+      //Specify initial position
+      marker.setLngLat([currpos.longitude, currpos.latitude]);
+      // console.log(tomMap);
+      marker.addTo(tomMap);
 
-                marker._element.addEventListener('click', ()=>{
-                    // console.log("Position...")
-                    // console.log([poiLoc.lng, poiLoc.lat]);
-                    // console.log(`${currpos.longitude},${currpos.latitude}:${poiLoc.lng},${poiLoc.lat}`)
-                    api.services.calculateRoute({
-                        key: process.env.REACT_APP_TT_API_KEY,
-                        locations: `${currpos.longitude},${currpos.latitude}:${poiLoc.lng},${poiLoc.lat}`,
-                        travelMode: 'bus'
-                      })
-                      .then(function(routeData) {
-                          const SECONDS = routeData.routes[0].summary.travelTimeInSeconds;
-                          const travelTime = new Date(SECONDS * 1000).toISOString().substr(11, 8);
-                          console.log("Travel time to " + poiName + ":" + travelTime);
-                          drawRoute(routeData.toGeoJson(), tomMap)
-                    });
+       //Add dragging interaction
+      marker.on('dragend', ()=>{
+        console.log("Finished moving");
+        console.log(marker.getLngLat());
+        const newPos = marker.getLngLat();
+        setZoomLevel(tomMap.getZoom())
+        console.log("Current zoom level" + zoomLevel)
+        setCurrpos({longitude:newPos.lng, latitude:newPos.lat});
+      });
+      marker.setPopup(popup).togglePopup();
+    };
+
+    //Adds markers for a given set of points
+    const addMulMarkers = (nearbyPois, tomMap) => {
+      if (nearbyPois.results.length > 0){
+        for (let pid in nearbyPois.results){
+          // console.log(nearbyPois.results[pid]);
+          const point = nearbyPois.results[pid];
+          //Unpack the point object to access the location
+          let poiLoc, poiName;
+          poiLoc = point.position
+          poiName = point.poi.name
+
+          // Popup
+          const popupOffset = {bottom : [0, -30]};
+          const popup = new tt.Popup({offset : popupOffset}).setHTML(poiName);
+          const markerElement = document.createElement('span', {className:''});
+          markerElement.innerHTML = icon({ prefix: 'fa', iconName: 'location-dot'}).html;
+          markerElement.className = 'marker-poi';
+          // markerElement.addEventListener('click', ()=>{
+          //     console.log("Position...")
+          //     console.log([poiLoc.lng, poiLoc.lat]);
+          // })
+          
+          const marker = new tt.Marker({
+              element:markerElement,
+              draggable:false
+          });
+  
+          //Specify initial position
+          marker.setLngLat([poiLoc.lng, poiLoc.lat]);
+          // console.log(marker)
+          marker.setPopup(popup);
+          marker.addTo(tomMap);
+
+          marker._element.addEventListener('click', ()=>{
+              // console.log("Position...")
+              // console.log([poiLoc.lng, poiLoc.lat]);
+              // console.log(`${currpos.longitude},${currpos.latitude}:${poiLoc.lng},${poiLoc.lat}`)
+              api.services.calculateRoute({
+                  key: process.env.REACT_APP_TT_API_KEY,
+                  locations: `${currpos.longitude},${currpos.latitude}:${poiLoc.lng},${poiLoc.lat}`,
+                  travelMode: 'bus'
                 })
-            }
-        }
+                .then(function(routeData) {
+                    const SECONDS = routeData.routes[0].summary.travelTimeInSeconds;
+                    const travelTime = new Date(SECONDS * 1000).toISOString().substr(11, 8);
+                    console.log("Travel time to " + poiName + ":" + travelTime);
+                    drawRoute(routeData.toGeoJson(), tomMap)
+              });
+          })
+        };
+      };
+    };
 
-        //Function to get and plot nearby points
-        async function getNearbyPoints(){
-            let res = await api.services.nearbySearch({
-                key: process.env.REACT_APP_TT_API_KEY,
-                center: [currpos.longitude,  currpos.latitude],
-                radius: 3000,
-                limit:100
-            })
-            // console.log(res)
-            if (res.results.length > 0) {
-                addMulMarkers(res); //Add all markers to map
-            }
-        }
+    //Function to get and plot nearby points
+  async function getNearbyPoints(){
+  let res = await api.services.nearbySearch({
+      key: process.env.REACT_APP_TT_API_KEY,
+      center: [currpos.longitude,  currpos.latitude],
+      radius: 3000,
+      limit:100
+    })
+    return res;
+  }
 
-        async function getNearbyPointsByCat(){
-            let res = await api.services.categorySearch({
-                key: process.env.REACT_APP_TT_API_KEY,
-                query:category,
-                center: [currpos.longitude,  currpos.latitude],
-                radius: 3000,
-                limit:10,
-                typeahead:false
-            })
-            // console.log(res)
-            if (res.results.length > 0) {
-                addMulMarkers(res); //Add all markers to map
-            }
-        }
+  async function getNearbyPointsByCat(){
+      let res = await api.services.categorySearch({
+          key: process.env.REACT_APP_TT_API_KEY,
+          query:category,
+          center: [currpos.longitude,  currpos.latitude],
+          radius: 3000,
+          limit:10,
+          typeahead:false
+      });
+      return res;
+  }
 
-      // console.log(mapOb)
-      getNearbyPointsByCat();
-
+    useEffect(async ()=>{
+      //Construct a map
+      const tomMap = createMap();
+      setMap(tomMap);
+      addMarker(tomMap);
+      const res = await getNearbyPointsByCat();
+      addMulMarkers(res, tomMap); //Add all markers to map
+  
       return () => tomMap.remove();
     }, [currpos, category]);
   
@@ -198,7 +192,7 @@ const Map = () => {
             {/* <input type="text" id="latlong" placeholder='49.266, -123.241' 
             onChange={(e)=>{parseLatLong(e.target.value)}}>
             </input> */}
-            {/* <input type="text" id="search-cat" placeholder='Burgers'></input> */}
+            <input type="text" id="search-cat" placeholder='Burgers'></input>
         </div>
         <div ref={mapElement} className='tomMap'> </div>
         </div>  
